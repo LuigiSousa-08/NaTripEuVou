bbbcc196e146ec8fbe95f02423bbb2726d593a30
LuigiSousa-08 <luigisousaa@gmail.com>
2024-03-07 11:13:50 -0300

    first commit

---
+++ app/src/main/java/com/example/natripeuvou/Login_Screen.kt
@@ -0,0 +1,11 @@
+package com.example.natripeuvou
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Login_Screen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_screen)
+    }
+}
